500cde97af93324c94f1d10f20162e5fec704a63
callee <240100889@qq.com>
2025-04-30 17:31:03 +0800

    Create copy_builds.bat

---
+++ copy_builds.bat
@@ -0,0 +1,12 @@
+copy "build\generic-legacy\web\viewer.mjs"  "E:\work\palmmmo3_editor_assets\pdf_china\web\viewer.mjs"
+copy "build\generic-legacy\web\viewer.html"  "E:\work\palmmmo3_editor_assets\pdf_china\web\viewer.html"
+copy "build\generic-legacy\web\viewer.css"  "E:\work\palmmmo3_editor_assets\pdf_china\web\viewer.css"
+copy "build\generic-legacy\build\pdf.mjs"         "E:\work\palmmmo3_editor_assets\pdf_china\libs\pdf.mjs"
+copy "build\generic-legacy\build\pdf.worker.mjs"  "E:\work\palmmmo3_editor_assets\pdf_china\libs\pdf.worker.mjs"
+
+copy "build\generic-legacy\web\viewer.mjs"  "E:\work\palmmmo3_editor_assets\pdf_global\web\viewer.mjs"
+copy "build\generic-legacy\web\viewer.html"  "E:\work\palmmmo3_editor_assets\pdf_global\web\viewer.html"
+copy "build\generic-legacy\web\viewer.css"  "E:\work\palmmmo3_editor_assets\pdf_global\web\viewer.css"
+copy "build\generic-legacy\build\pdf.mjs"         "E:\work\palmmmo3_editor_assets\pdf_global\libs\pdf.mjs"
+copy "build\generic-legacy\build\pdf.worker.mjs"  "E:\work\palmmmo3_editor_assets\pdf_global\libs\pdf.worker.mjs"
+
