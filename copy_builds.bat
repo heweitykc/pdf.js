5f67c5c1eb486e7ed9e0b3eb4c64c0a0f0dbe706
callee <240100889@qq.com>
2025-07-08 17:02:47 +0800

    Update copy_builds.bat

--- copy_builds.bat
+++ copy_builds.bat
@@ -1,9 +1,17 @@
+xcopy "build\generic-legacy\web\locale\zh-CN"  "E:\work\palmmmo3_editor_assets\pdf_china\web\locale\zh-CN" /E /I /Y
+xcopy "build\generic-legacy\web\locale\en-US"  "E:\work\palmmmo3_editor_assets\pdf_china\web\locale\en-US" /E /I /Y
+xcopy "build\generic-legacy\web\images"  "E:\work\palmmmo3_editor_assets\pdf_china\web\images" /E /I /Y
+copy "build\generic-legacy\web\palmmob3.js"  "E:\work\palmmmo3_editor_assets\pdf_china\web\palmmob3.js"
 copy "build\generic-legacy\web\viewer.mjs"  "E:\work\palmmmo3_editor_assets\pdf_china\web\viewer.mjs"
 copy "build\generic-legacy\web\viewer.html"  "E:\work\palmmmo3_editor_assets\pdf_china\web\viewer.html"
 copy "build\generic-legacy\web\viewer.css"  "E:\work\palmmmo3_editor_assets\pdf_china\web\viewer.css"
 copy "build\generic-legacy\build\pdf.mjs"         "E:\work\palmmmo3_editor_assets\pdf_china\libs\pdf.mjs"
 copy "build\generic-legacy\build\pdf.worker.mjs"  "E:\work\palmmmo3_editor_assets\pdf_china\libs\pdf.worker.mjs"
 
+
+xcopy "build\generic-legacy\web\locale"  "E:\work\palmmmo3_editor_assets\pdf_global\web\locale" /E /I /Y
+xcopy "build\generic-legacy\web\images"  "E:\work\palmmmo3_editor_assets\pdf_global\web\images" /E /I /Y
+copy "build\generic-legacy\web\palmmob3.js"  "E:\work\palmmmo3_editor_assets\pdf_global\web\palmmob3.js"
 copy "build\generic-legacy\web\viewer.mjs"  "E:\work\palmmmo3_editor_assets\pdf_global\web\viewer.mjs"
 copy "build\generic-legacy\web\viewer.html"  "E:\work\palmmmo3_editor_assets\pdf_global\web\viewer.html"
 copy "build\generic-legacy\web\viewer.css"  "E:\work\palmmmo3_editor_assets\pdf_global\web\viewer.css"
